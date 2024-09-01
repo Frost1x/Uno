ebf23e29f8d17fe607e8812c06da31568b7279b9
Frost1x <103965503+Frost1x@users.noreply.github.com>
2024-08-31 23:05:39 -0700

    Update Uno.py

--- Uno.py
+++ Uno.py
@@ -1,5 +1,5 @@
 import random
-#solved Think about a way to check if player cards matches the same color. NOW WALKTHROUGH THE CODE.
+#updated if currentCard matches player1 and player2
 def playUno():
     redCard = ["red0", "red1", "red2", "red3", "red4", "red5", "red6", "red7", "red8", "red9", "redReverse", "redPlus2"]
     yellowCard = ["yellow0", "yellow1", "yellow2", "yellow3", "yellow4", "yellow5", "yellow6", "yellow7", "yellow8",
@@ -14,82 +14,90 @@ def playUno():
     amountCards = 7
     player1Cards = Player1(allCards, amountCards)
     player2Cards = Player2(allCards, amountCards)
-    currentValidCard = random.choice(allCards)
-    print("Current card:", currentValidCard)
-    while True: 
+    currentCard = random.choice(allCards)
+    
+    while True:
+        if currentCard.startswith("red"):
+            currentColor = "red"
+        elif currentCard.startswith("yellow"):
+            currentColor = "yellow"
+        elif currentCard.startswith("green"):
+            currentColor = "green"
+        elif currentCard.startswith("blue"):
+            currentColor = "blue"
+   
+        print("Current Card:", currentCard)
+    
+        player1 = input("Player 1, throw in a card: ")
+        if player1.startswith("red"):
+            player1Color = "red"
+        elif player1.startswith("yellow"):
+            player1Color = "yellow"
+        elif player1.startswith("green"):
+            player1Color = "green"
+        elif player1.startswith("blue"):
+            player1Color = "blue"
+
         
-        player1 = input("Player 1, Throw in a card: ")
+        if player1Color == currentColor or player1.startswith("plus4cards"):
+            print('Player 1: True')
+        else:
+            print('Player 1: False')
+            return
+        
         if player1 in player1Cards:
-            if validateMove(player1Cards, currentValidCard):
-                player1Cards = removeCard(player1Cards, player1Cards)
-                print("Player 1 your cards are:", player1Cards)
-                currentValidCard = player1Card
-                if player1Card in specialCard:
-                    specialCardAction(player1Cards, allCards)
-            else:
-                print("Invalid Card. Try again.")
+            player1Cards = removeCard(player1Cards, allCards, player1)
+            print("Player 1 your cards are:", player1Cards)
         else:
-            print("Card not in hand. Try again.")
-         # Player 2's turn
-        player2Card = input("Player 2, Throw in a card: ")
-        if player2Card in player2Cards:
-            if validateMove(player2Cards, currentValidCard):
-                player2Cards = removeCard(player2Cards, allCards, player2Cards)
-                print("Player 2 your cards are:", player2Cards)
-                currentValidCard = player2Cards
-                if player2Cards in specialCard:
-                    specialCardAction(player2Cards, allCards)
-            else:
-                print("Invalid Card. Try again.")
+            print("Card not found in Player 1's hand")
+        
+
+        print("Current Card:", currentCard)
+        
+        player2 = input("Player 2, throw in a card: ")
+        if player2.startswith("red"):
+            player2Color = "red"
+        elif player2.startswith("yellow"):
+            player2Color = "yellow"
+        elif player2.startswith("green"):
+            player2Color = "green"
+        elif player2.startswith("blue"):
+            player2Color = "blue"
         else:
-            print("Card not in hand. Try again.")
-            
-def validateMove(card, currentValidCard):
-    if card == "plus4cards":
-        return card == currentValidCard
-    
-    cardColor, cardType = card[:-1], card[-1]
-    currentColor, currentType = currentValidCard[:-1], currentValidCard[-1]
-    
-    return cardColor == currentColor or cardType == currentType
+            player2Color = None
 
+        if player2Color == currentColor or player2.startswith("plus4cards"):
+            print('Player 2: True')
+        else:
+            print('Player 2: False')
+            return
+        
+        if player2 in player2Cards:
+            player2Cards = removeCard(player2Cards, allCards, player2)
+            print("Player 2 your cards are:", player2Cards)
+        else:
+            print("Card not found in Player 2's hand")
+        
 
-def removeCard(player1Cards, player2Cards , allCards, player1):
-    if player1 in player1Cards:
-        player1Cards.remove(player1)
-        if player1 in allCards:
-            allCards.remove(player1)
-    return player1Cards
-    
-    if player2 in player2Cards:
-        player2Cards.remove(player2)
-        if player2 in allCards:
-            allCards.remove(player2)
-    return player2Cards
+def removeCard(playerCards, allCards, cardToRemove):
+    if cardToRemove in playerCards:
+        playerCards.remove(cardToRemove)
+    if cardToRemove in allCards:
+        allCards.remove(cardToRemove)
+    return playerCards
 
-def specialCard1(player1, allCards):
-    if player1 == "redPlus2":
+def specialCard(card, allCards):
+    if card == "redPlus2":
         redPlus(allCards)
-    elif player1 == "yellowPlus2":
+    elif card == "yellowPlus2":
         yellowPlus(allCards)
-    elif player1 == "greenPlus2":
+    elif card == "greenPlus2":
         greenPlus(allCards)
-    elif player1 == "bluePlus2":
+    elif card == "bluePlus2":
         bluePlus(allCards)
-    elif player1 == "plus4cards":
+    elif card == "plus4cards":
         plus4(allCards)
 
-def specialCard2(player2, allCards):
-    if player2 == "redPlus2":
-        redPlus(allCards)
-    elif player2 == "yellowPlus2":
-        yellowPlus(allCards)
-    elif player2 == "greenPlus2":
-        greenPlus(allCards)
-    elif player2 == "bluePlus2":
-        bluePlus(allCards)
-    elif player2 == "plus4cards":
-        plus4(allCards)
 
 def redPlus(allCards):
     red = random.sample(allCards, 2)
@@ -117,18 +125,20 @@ def plus4(allCards):
 
 
 def Player1(allCards, amountCards):
-    picker = random.sample(allCards, amountCards)
-    print("Player1 your cards are", picker)
-    return picker
+    picker1 = random.sample(allCards, amountCards)
+    print("Player 1 your cards are", picker1)
+    return picker1
 
 def Player2(allCards, amountCards):
-    picker = random.sample(allCards, amountCards)
-    print("Player2 your cards are", picker)
-    return picker
+    picker2 = random.sample(allCards, amountCards)
+    print("Player 2 your cards are", picker2)
+    return picker2
+
 
 def main():
     playUno()
-    
+
+
 if __name__ == "__main__":
     main()
-# not finished yet
+
